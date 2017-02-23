ff49447ee8eef6617b8de3ae89c0d5b4bfe12885
Robert Brice <rob.a.brice@gmail.comC>
2017-02-23 14:48:34 +0000

    tables now created correctly

--- db/cashboard.sql
+++ db/cashboard.sql
@@ -4,21 +4,18 @@ DROP TABLE categories;
 
 CREATE TABLE categories(
 id SERIAL8 primary key,
-category_name VARCHAR(255),
+category_name VARCHAR(255)
 );
 
 CREATE TABLE users(
 id SERIAL8 primary key,
 name VARCHAR(255),
-monthly_budget NUMERIC(6,2),
+monthly_budget NUMERIC(6,2)
 );
 
 CREATE TABLE transactions(
 id SERIAL8 primary key,
 merchant_name VARCHAR(255),
 amount NUMERIC(6,2),
-category_id references categories(id) ON DELETE CASCADE
-user_id references users(id) ON DELETE CASCADE
-);
-
-
+category_id INT8 references categories(id) ON DELETE CASCADE,
+user_id INT8 references users(id) ON DELETE CASCADE);
