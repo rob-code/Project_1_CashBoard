ac7fb8a8343feb3871f9b60129184b8534867d30
Robert Brice <rob.a.brice@gmail.comC>
2017-02-27 22:27:42 +0000

    changed database number settings to accommodate larger numbers

--- db/cashboard.sql
+++ db/cashboard.sql
@@ -18,20 +18,20 @@ second_name VARCHAR(255)
 CREATE TABLE transactions(
 id SERIAL8 primary key,
 merchant_name VARCHAR(255),
-amount NUMERIC(6,2),
+amount NUMERIC(12,2),
 category_id INT8 references categories(id) ON DELETE CASCADE,
 user_id INT8 references users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE targets(
 id SERIAL8 primary key,
-amount NUMERIC(6,2),
+amount NUMERIC(12,2),
 category_id INT8 references categories(id) ON DELETE CASCADE,
 user_id INT8 references users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE budgets(
 id SERIAL8 primary key,
-total_budget NUMERIC(6,2),
+total_budget NUMERIC(12,2),
 user_id INT8 references users(id) ON DELETE CASCADE
 );
