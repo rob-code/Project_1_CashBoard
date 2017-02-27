d688c816a890baef578c892f4f9073c59b8332a3
Robert Brice <rob.a.brice@gmail.comC>
2017-02-27 14:11:26 +0000

    displaying all transactions with category header

--- app_controller.rb
+++ app_controller.rb
@@ -8,39 +8,45 @@ require('./models/target.rb')
 require('./models/transaction.rb')
 
 get '/landing' do
-erb(:landing)
+  erb(:landing)
 end
 
 get '/login' do
-@users = User.all
-erb(:index)
+  @users = User.all
+  erb(:index)
 end
 
 get '/login/:id' do
-@user = User.return_by_id(params[:id])
-erb(:dashboard)
+  @user = User.return_by_id(params[:id])
+  erb(:dashboard)
 end
 
 get '/new_transaction/:id' do
-@user = User.return_by_id(params[:id])
-@categories = Category.all
-erb(:new_transaction)
+  @user = User.return_by_id(params[:id])
+  @categories = Category.all
+  erb(:new_transaction)
 end
 
 post '/new_transaction/' do
-@transaction = Transaction.new(params)
-@transaction.save()
-@user = User.return_by_id(params[:user_id])
-@category = Category.return_by_id(params[:category_id])
-erb(:transaction_created)
+  @transaction = Transaction.new(params)
+  @transaction.save()
+  @user = User.return_by_id(params[:user_id])
+  @category = Category.return_by_id(params[:category_id])
+  erb(:transaction_created)
 end
 
+get '/all_transactions/:id' do
+  @user = User.return_by_id(params[:id])
+  @transactions = Transaction.all_for_user(params[:id])
+  @categories = Category.all
+  erb(:all_transactions)
+end
 
 get '/sub_cat/:id/:cat_id' do
   @user = User.return_by_id(params[:id])
   @transactions = Transaction.subcategories(params[:id], params[:cat_id])
   @category = Category.return_by_id(params[:cat_id])
-erb(:category)
+  erb(:category)
 end
 
 
